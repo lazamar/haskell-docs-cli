ce7c41f05ff521cffe4b48cc753f42b8f096f7c3
Marcelo Lazaroni <lazamar@fb.com>
2021-08-27 23:34:28 +0100

    Add cram tests to CI

--- test/cram/run.sh
+++ test/cram/run.sh
@@ -5,7 +5,8 @@
 # path to the directory where the test file lives.
 # This means that all files must be in the same directory as this runner.
 
-$TESTDIR/../../.stack-work/dist/x86_64-osx/Cabal-3.2.1.0/build/haskell-docs-cli-exe/haskell-docs-cli-exe \
+cd $TESTDIR/../..
+$(stack path --dist-dir)/build/hdc/hdc \
   --data-dir $TESTDIR/cram-data \
   --unlimited-cache \
   $@
